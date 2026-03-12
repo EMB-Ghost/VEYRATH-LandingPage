e131ba019cb5e47dccb3fdf1c84be01ffbcda447
Embrasa Games <157664383+EMB-Ghost@users.noreply.github.com>
2026-03-12 10:05:39 -0300

    Update Countdown component and package image display

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -285,7 +285,7 @@ export default function Home() {
           <p>{preorder.subtitle}</p>
         </div>
 
-        <Countdown duration={preorder.countdownDuration} />
+        <Countdown />
 
         <div className="packages" data-reveal>
           {preorder.packages.map((pkg) => (
@@ -294,7 +294,9 @@ export default function Home() {
               {pkg.featured && pkg.featuredLabel && (
                 <div className="pkg-label">{pkg.featuredLabel}</div>
               )}
-              <div className="pkg-art">{pkg.emoji}</div>
+              <div className="pkg-art">
+                <img src={pkg.image} alt={pkg.name} />
+              </div>
               <div className="pkg-body">
                 <div className="pkg-tier">{pkg.tier}</div>
                 <div className="pkg-name">{pkg.name}</div>
